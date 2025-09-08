c387f0a4f8ca0409acbfa457a42444e691e806b6
ibrahim-ahmed201 <ibrahimahmed384888@gmail.com>
2025-09-08 18:34:17 +0300

    Creating A backend Using SupaBase And Already connected to the Application

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,10 +1,16 @@
+import { useEffect } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import { getCabins } from "../services/apiCabins";
 
 function Cabins() {
+  useEffect(function () {
+    getCabins().then((data) => console.log(data));
+  }, []);
   return (
     <Row type="horizontal">
       <Heading as="h1">All cabins</Heading>
+      <img src="https://qkfipiszaklwcwnlnruv.supabase.co/storage/v1/object/public/Cabins/cabin-001.jpg" />
       <p>TEST</p>
     </Row>
   );
---
+++ src/services/supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://qkfipiszaklwcwnlnruv.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFrZmlwaXN6YWtsd2N3bmxucnV2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTcxOTYyNTksImV4cCI6MjA3Mjc3MjI1OX0.rM7yIULZbaSxP2tD4VByItbmi26qtJZooJGmGZia3-Y";
+const supabase = createClient(supabaseUrl, supabaseKey);
+export default supabase;
